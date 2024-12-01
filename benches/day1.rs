1fb9e0b6cb27a8791bcac151f3c0abf3b477e81f
Thomas Aubry <thomas.aubry@paylead.fr>
2024-12-01 16:11:57 +0100

    feat: input as env secret for a working CI

--- benches/day1.rs
+++ benches/day1.rs
@@ -2,7 +2,12 @@ use aoc_2024::day1::{part1, part2};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let input = include_str!("../input/2024/day01.txt");
+    let input = match std::env::var("AOC_INPUT_DAY01") {
+        Ok(val) => val,
+        Err(_) => include_str!("../input/2024/sample/day1.txt").to_owned(),
+    };
+    let input = input.as_str();
+
     c.bench_function("part1", |b| b.iter(|| part1(black_box(input))));
     c.bench_function("part2", |b| b.iter(|| part2(black_box(input))));
 }
